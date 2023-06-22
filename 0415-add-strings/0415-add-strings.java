338ef4ef0aff4c31f5bdf43236aab6cf63a08e42
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2023-06-23 00:30:13 +0530

    Time: 2 ms (98.12%), Space: 41.8 MB (76.40%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,10 +1,21 @@
-import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger i=new BigInteger(num1);
-        BigInteger j=new BigInteger(num2);
-        BigInteger x =i.add(j);
-        String s = x.toString(); 
-        return s;
+        StringBuilder sb = new StringBuilder();
+
+        int i=num1.length() - 1, j=num2.length() - 1;
+        int carry=0;
+
+        while(i>=0 || j>=0) {
+            int sum=carry;
+
+            if(i>=0) sum += (num1.charAt(i--) - '0');
+            if(j>=0) sum += (num2.charAt(j--) - '0');
+
+            sb.append(sum%10);
+            carry=sum/10;
+        }
+
+        if(carry!=0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
